8b483955ed8012686fc437d9c9b8aacac5269eb2
Faisca <Faisca@DESKTOP-JTTHR46>
2018-07-03 23:16:13 -0300

    Sigleton

---
+++ Criacao/Sigleton/SigletonFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Criacao.Sigleton
+{
+    class SigletonFactory
+    {
+        public SigletonFactory sigletonFactory { get; set; }
+        private SigletonFactory()
+        {
+        }
+
+        public SigletonFactory Crie()
+        {
+            return sigletonFactory ?? new SigletonFactory();
+        }
+    }
+}
