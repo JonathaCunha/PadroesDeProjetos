c1b16332dfaa3e22ce0a4227b7917d91b6817043
Faisca <Faisca@DESKTOP-JTTHR46>
2021-02-25 21:08:22 -0300

    novo event observe com Func<>

--- Comportamental/Observer/VersaoCSharp6/Observada2.cs
+++ Comportamental/Observer/VersaoCSharp6/Observada2.cs
@@ -7,6 +7,7 @@ namespace Comportamental.Observer.VersaoCSharp6
     public class Observada2
     {
         public event Action Update;
+        //public event Func<int, string> Update2;
 
         public void Notify() => Update?.Invoke();
     }
